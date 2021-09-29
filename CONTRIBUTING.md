acb68d7abafb80058d7f2414432275f48ddf42f4
Grégoire Dubourg <gregoire.dubourg@linkfluence.com>
2021-09-29 23:19:27 +0200

    Add tailwind CSS

---
+++ CONTRIBUTING.md
@@ -0,0 +1,19 @@
+# TODO
+
+- [x] Déployer l'app sur Netlify
+- [ ] Utiliser Tailwind CSS
+- [ ] Faire un favicon
+- [ ] Ajouter des statistiques (bonnes réponses, mauvaises réponses, temps de réponse, évolution)
+- [ ] Faire un mode chronométré
+- [ ] Stocker l'historique des réponses par opération (pour voir l'évolution)
+- [ ] Mettre au point un score (en fonction du nombre de fautes, de la vitesse…)
+- [ ] Gérer les question à reposer en question du temps de réponse, du nombre d'erreurs…
+- [ ] Stockage local
+- [ ] Identification avec stockage sur serveur
+- [ ] Design plus fun
+  - [ ] Arrière-plan qui change
+  - [ ] Mettre des émojis
+  - [ ] Faire des animations
+- [ ] Possibilité de personnaliser les opérations (choix des opérandes, des opérateurs, réponses composées (division euclidienne), réponses autres que numériques (QCM, fractions…))
+- [ ] Monétiser l'app
+- [ ] Ajouter des badges de progression
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { SetStateAction, useState } from "react";
 import { createOperation, Operation, solveOperation } from "./models/operation";
 
@@ -50,25 +49,25 @@ const App = () => {
   };
 
   return (
-    <div className="app">
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center flex-col gap-y-4">
       <p>Deviens super balaise en calcul mental ! En t'amusant…</p>
       <p>Score : {score}</p>
       {operation && (
-        <p>
+        <p className="text-3xl text-gray-600 font-bold">
           {operation.operand1} {operation.operator} {operation.operand2}
         </p>
       )}
-      <p>
-        <input
-          className="user-answer"
-          placeholder="?"
-          type="text"
-          autoFocus
-          onKeyPress={handleKeyPress}
-          value={inputValue}
-          onChange={handleChange}
-        />
-      </p>
+      <input
+        className="text-center text-5xl shadow-xl rounded-2xl focus:outline-none focus:ring focus:border-purple-500 p-6 box-content"
+        size={1}
+        style={{ width: inputValue.length * 40, minWidth: "1em" }}
+        placeholder="?"
+        type="text"
+        autoFocus
+        onKeyPress={handleKeyPress}
+        value={inputValue}
+        onChange={handleChange}
+      />
       <p>
         {status === Status.Success
           ? "✅ Bonne réponse !"
